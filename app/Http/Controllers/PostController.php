82f379a4d6efcea3ade24595fc3fa30639a3b146
Dmitry <funbrainfun@gmail.com>
2024-06-16 12:57:54 +0300

    setup

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(){
+        return inertia('Post/Index');
+    }
+
+    public function create(){
+        return inertia('Post/Create');
+    }
+    public function store(){
+       
+    }
+}
---
+++ resources/js/Pages/Post/Create.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="w-96 mx-auto pt-8">
+        <h1 class="text-lg mb-8">Create</h1>
+        <form @submit.prevent="store">
+            <div><input v-model="title" type="text" placeholder="title" /></div>
+            <div>
+                <textarea v-model="content" placeholder="content"></textarea>
+            </div>
+            <div>
+                <button
+                    class="hover:bg-white hover:text-sky-500 block p-2 w-32 border border-sky-500 bg-sky-500 rounded-full text-center text-white"
+                    type="submit"
+                >
+                    Store
+                </button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Create",
+    data() {
+        return {
+            title: "",
+            content: "",
+        };
+    },
+    methods: {
+        store() {
+            this.$inertia.post("/posts", {});
+        },
+    },
+};
+</script>
+
+<style scoped></style>
